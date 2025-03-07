03392ef6415c07d1495f519e0c5cba03d5056edc
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2025-03-07 23:45:11 +0100

    window context

--- src/context.py
+++ src/context.py
@@ -9,45 +9,59 @@ pygame.display.set_mode((800, 600), pygame.DOUBLEBUF | pygame.OPENGL)
 gluPerspective(45, (800 / 600), 0.1, 50.0)
 glTranslatef(0.0, 0.0, -5.0)
 
-verticies = (
-    (1, -1, -1),
-    (1, 1, -1),
-    (-1, 1, -1),
-    (-1, -1, -1),
-    (1, -1, 1),
-    (1, 1, 1),
-    (-1, -1, 1),
-    (-1, 1, 1)
-)
-edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
-    (5,1),
-    (5,4),
-    (5,7)
-)
+class Context:
+    def __init__(self, width, height, name):
+        self.width = width
+        self.height = height
+        self.name = name
+        self._init()
+        self.is_running = True
+        self.meshes = []
 
-is_running = True
+    def _init(self):
+        pygame.init()
+        pygame.mixer.init()
+        window = pygame.display.set_mode((self.width, self.height), pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE)
+        pygame.display.set_caption("Powered by XYCORP Labs") # self.name
 
-while is_running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: is_running = False
+    def run(self):
+        while self.is_running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: is_running = False
 
-    glRotatef(1, 3, 1, 1)
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+            glRotatef(1, 3, 1, 1)
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-    glBegin(GL_LINES)
-    for e in edges:
-        for v in e:
-            glVertex3fv(verticies[v])
-    glEnd()
+            glBegin(GL_LINES)
+            # for e in edges:
+            #     for v in e:
+            #         glVertex3fv(verticies[v])
+            glEnd()
 
-    pygame.display.flip()
-    pygame.time.wait(10)
+            pygame.display.flip()
+            pygame.time.wait(10)
+
+# verticies = (
+#     (1, -1, -1),
+#     (1, 1, -1),
+#     (-1, 1, -1),
+#     (-1, -1, -1),
+#     (1, -1, 1),
+#     (1, 1, 1),
+#     (-1, -1, 1),
+#     (-1, 1, 1)
+# )
+# edges = (
+#     (0,1),
+#     (0,3),
+#     (0,4),
+#     (2,1),
+#     (2,3),
+#     (2,7),
+#     (6,3),
+#     (6,4),
+#     (6,7),
+#     (5,1),
+#     (5,4),
+#     (5,7)
+# )
