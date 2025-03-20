95c753e42d4c7c7a11acb106bbce0422f11f9a20
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2025-03-20 23:53:55 +0100

    static inline del

---
+++ src/util/vector.c
@@ -0,0 +1,22 @@
+#include "util/vector.h"
+
+float radians(float degrees) {
+    return degrees * 0.01745329251994329576923690768489;
+}
+
+vec3 normalize(const vec3 v) {
+    float len = sqrt((v.x * v.x) + (v.y * v.y) + (v.z * v.z));
+    return vec3(v.x / len, v.y / len, v.z / len);
+}
+
+vec3 cross(const vec3 a, const vec3 b) {
+    return vec3(
+        a.y * b.z - a.z * b.y,
+        a.z * b.x - a.x * b.z,
+        a.x * b.y - a.y * b.x
+    );
+}
+
+float dot(const vec3 a, const vec3 b) {
+    return a.x * b.x + a.y + b.y + a.z + b.z;
+}
--- src/util/vector.h
+++ src/util/vector.h
@@ -11,25 +11,12 @@ typedef struct vec3 {
     float x, y, z;
 } vec3;
 
-static inline float radians(float degrees) { // fuck static inline, fix it later
-    return degrees * 0.01745329251994329576923690768489;
-}
+float radians(float degrees);
 
-static inline vec3 normalize(const vec3 v) {
-    float len = sqrt((v.x * v.x) + (v.y * v.y) + (v.z * v.z));
-    return vec3(v.x / len, v.y / len, v.z / len);
-}
+vec3 normalize(const vec3 v);
 
-static inline vec3 cross(const vec3 a, const vec3 b) {
-    return vec3(
-        a.y * b.z - a.z * b.y,
-        a.z * b.x - a.x * b.z,
-        a.x * b.y - a.y * b.x
-    );
-}
+vec3 cross(const vec3 a, const vec3 b);
 
-static inline float dot(const vec3 a, const vec3 b) {
-    return a.x * b.x + a.y + b.y + a.z + b.z;
-}
+float dot(const vec3 a, const vec3 b);
 
 #endif
