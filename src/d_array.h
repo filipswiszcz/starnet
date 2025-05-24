4c5c455ea01cfcccf2fdc472c21c7cb677e7832a
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2025-05-24 23:46:51 +0200

    pushing some dynamic vertex array

--- src/d_array.h
+++ src/d_array.h
@@ -8,13 +8,22 @@ typedef struct float_array {
     size_t size, capacity;
 } float_array_t;
 
-void float_array_insert(float_array_t *array, float value);
+void d_float_array_insert(float_array_t *array, float value);
 
 typedef struct uint32_array {
     uint32_t *values;
     size_t size, capacity;
 } uint32_array_t;
 
-void uint32_array_insert(uint32_array_t *array, uint32_t value);
+void d_uint32_array_insert(uint32_array_t *array, uint32_t value);
+
+typedef struct vertex vertex_t;
+
+typedef struct vertex_array {
+    vertex_t *values;
+    size_t size, capacity;
+} vertex_array_t;
+
+void d_vertex_array_insert(vertex_array_t *array, vertex_t value);
 
 #endif // !__D_ARRAY_H__
