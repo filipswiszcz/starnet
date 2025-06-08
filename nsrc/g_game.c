e296fc6fc38e3a69b22baf0ddc8b3ef43f5e5835
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2025-06-08 22:36:26 +0200

    keyboard rush

--- nsrc/g_game.c
+++ nsrc/g_game.c
@@ -6,9 +6,65 @@
 
 #define WINDOW_NAME "SOLAR (Build v0.0.8)"
 
-void g_mouse_input() {}
+// INPUTS
 
-void g_keyboard_input() {}
+int IS_FIRST_PLAY = 1;
+
+void g_mouse_input() {
+    double mouse_x, mouse_y;
+    glfwGetCursorPos(context.window, &mouse_x, &mouse_y);
+
+    if (IS_FIRST_PLAY) {
+        context.camera.mouse_x = mouse_x;
+        context.camera.mouse_y = mouse_y;
+        IS_FIRST_PLAY = 0;
+    }
+
+    float offset_x = mouse_x - context.camera.mouse_x;
+    float offset_y = context.camera.mouse_y - mouse_y;
+
+    context.camera.mouse_x = mouse_x;
+    context.camera.mouse_y = mouse_y;
+
+    offset_x *= context.camera.sensitivity;
+    offset_y *= context.camera.sensitivity;
+
+    context.camera.yaw = (context.camera.yaw + offset_x);
+    context.camera.pitch = (context.camera.pitch + offset_y);
+
+    if (context.camera.pitch > 89.0f) context.camera.pitch = 89.0f;
+    if (context.camera.pitch < -89.0f) context.camera.pitch = -89.0f;
+
+    vec3_t target = {
+        cos(r_radians(context.camera.yaw)) * cos(r_radians(context.camera.pitch)),
+        sin(r_radians(context.camera.pitch)),
+        sin(r_radians(context.camera.yaw)) * cos(r_radians(context.camera.pitch))
+    };
+
+    context.camera.target_position = r_normalize(target);
+}
+
+float CAMERA_TIME_BETWEEN_FRAMES = 0.0f;
+float CAMERA_TIME_OF_LAST_FRAME = 0.0f;
+
+void g_keyboard_input() {
+    if (glfwGetKey(context.window, GLFW_KEY_W) == GLFW_PRESS)
+        context.camera.position = vec3_add(context.camera.position, vec3_mul(context.camera.target_position, (context.camera.speed * CAMERA_TIME_BETWEEN_FRAMES))); 
+    if (glfwGetKey(context.window, GLFW_KEY_S) == GLFW_PRESS)
+        context.camera.position = vec3_sub(context.camera.position, vec3_mul(context.camera.target_position, (context.camera.speed * CAMERA_TIME_BETWEEN_FRAMES))); 
+    if (glfwGetKey(context.window, GLFW_KEY_A) == GLFW_PRESS) 
+        context.camera.position = vec3_sub(context.camera.position, vec3_mul(r_normalize(r_cross(context.camera.target_position, context.camera.head_position)), (context.camera.speed * CAMERA_TIME_BETWEEN_FRAMES)));
+    if (glfwGetKey(context.window, GLFW_KEY_D) == GLFW_PRESS)
+        context.camera.position = vec3_add(context.camera.position, vec3_mul(r_normalize(r_cross(context.camera.target_position, context.camera.head_position)), (context.camera.speed * CAMERA_TIME_BETWEEN_FRAMES)));
+    if (glfwGetKey(context.window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+        context.camera.speed = 8i.0f;
+    if (glfwGetKey(context.window, GLFW_KEY_LEFT_SHIFT) == GLFW_RELEASE)
+        context.camera.speed = 2.0f;
+    if (glfwGetKey(context.window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(context.window, true);
+}
+
+// FRAMES
 
 float TIME_OF_LAST_FRAME = 0.0f;
 int FRAMES_PER_SECOND = 0;
@@ -26,6 +82,8 @@ void g_game_frames() {
         TIME_OF_LAST_FRAME = 1.0f;
     }
 }
+
+// GAME
 
 void g_game_init() {
     ASSERT(glfwInit(), "Failed to initialize OpenGL");
@@ -51,6 +109,8 @@ void g_game_init() {
 void g_game_loop() {
     while (!glfwWindowShouldClose(context.window)) {
         // inputs
+        g_mouse_input();
+        g_keyboard_input();
 
         // frames updater
         g_game_frames();
