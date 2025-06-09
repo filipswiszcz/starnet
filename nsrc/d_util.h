db5c6ca0206e9a4a8209979f0e1dd6ffe4f483ca
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2025-06-09 23:48:09 +0200

    i_main was empty and sad

--- nsrc/d_util.h
+++ nsrc/d_util.h
@@ -1,6 +1,8 @@
 #ifndef __D_UTIL_H__
 #define __D_UTIL_H__
 
+#include <stdlib.h>
+
 #define ASSERT(_e, ...) if (!(_e)) {fprintf(stderr, __VA_ARGS__); exit(1);}
 
 #endif // !__D_UTIL_H__
--- nsrc/i_main.c
+++ nsrc/i_main.c
@@ -1,3 +1,8 @@
+#include "g_game.h"
+
 int main(int argc, char **argv) {
+  g_game_init();
+  g_game_loop();
+  g_game_stop();
   return 0;
 }
